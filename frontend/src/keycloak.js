571ba31839dc208a3113162238bbd701ac87e20b
Ondřej Švarc <ondrej.svarc@czechitas.cz>
2025-12-28 12:28:18 +0100

    fixed ports and ips

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 // Use environment variables for Keycloak configuration for flexibility
 const keycloakConfig = {
-    url: import.meta.env.VITE_KEYCLOAK_URL || 'http://192.168.1.112:8080',
+    url: import.meta.env.VITE_KEYCLOAK_URL || 'http://localhost:8081',
     realm: import.meta.env.VITE_KEYCLOAK_REALM || 'SPSHPAU',
     clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID || 'spshpau-rest-api',
 };
